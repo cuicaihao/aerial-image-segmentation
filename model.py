c264cea139d84a36a72816bead9ae1b90c939e9f
chris cui <caihao.cui@ieee.org>
2021-01-24 20:08:00 +1100

    apply Adam method; update nn structure with atrous layer

--- model.py
+++ model.py
@@ -11,19 +11,22 @@ class FCNN(nn.Module):
     def __init__(self, num_classes=2):
         super(FCNN, self).__init__()
         # 1 input image channel, 6 output channels, 3x3 square convolution
-        # kernel
+        # kernel :  N C H W
         # https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html?highlight=nn%20conv2d#torch.nn.Conv2d
         nc = num_classes
         self.conv1 = nn.Conv2d(3, 16, (3, 3), padding=1)
-        self.conv2 = nn.Conv2d(16, 32, (3, 3), padding=1)
+        # add  à trous algorithm, looking large area and keep in and out same size.
+        self.convx = nn.Conv2d(3, 16, (5, 5), stride=1, padding=4, dilation=2)
+
+        self.conv2 = nn.Conv2d(16 + 16, 32, (3, 3), padding=1)
         self.conv3 = nn.Conv2d(32, 16, (3, 3), padding=1)
         self.conv4 = nn.Conv2d(16, nc, (5, 5), padding=2)
 
     def forward(self, x):
         input_size = x.size()[2:]
-
-        x = self.conv1(x)
-        x = F.relu(x)
+        x1 = F.relu(self.conv1(x))
+        x2 = F.relu(self.convx(x))
+        x = torch.cat((x1, x2), 1)  # concatenated on channel
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, (2, 2))
--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 # -*- utf-8: python -*-
-'''
+"""
 Software Design
 
 Authoer: Chris Cui
 
 Time: 2019-09-03
-'''
+"""
 
 import torch
 import torch.optim
@@ -16,20 +16,36 @@ from model import FCNN
 from loss import CrossEntropyLoss2d
 
 
-def train(model, train_loader, device,
-          tile_size, epochs=10, batch_size=1,
-          learning_rate = 1e-4, momentum=0.9, weight_decay=5e-3):
+def train(
+    model,
+    train_loader,
+    device,
+    tile_size,
+    epochs=10,
+    batch_size=1,
+    learning_rate=1e-4,
+    momentum=0.9,
+    weight_decay=5e-3,
+):
     criterion = CrossEntropyLoss2d()
-    optimizer = torch.optim.SGD(model.parameters(
-    ), lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
+
+    # optimizer = torch.optim.SGD(
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=learning_rate,
+        # momentum=momentum,
+        weight_decay=weight_decay,
+    )
     model.train()
     model = model.to(device=device)
     criterion = criterion.to(device=device)
     training_stats = utils.Stats()
     for n in range(epochs):
         epoch_stats = utils.Stats()
-        loader_with_progress = utils.loader_with_progress(train_loader,
-                                                          epoch_n=n, epoch_total=epochs, stats=epoch_stats, leave=True)
+
+        loader_with_progress = utils.loader_with_progress(
+            train_loader, epoch_n=n, epoch_total=epochs, stats=epoch_stats, leave=True
+        )
         for i, (x, y) in enumerate(loader_with_progress):
             y = y.to(device=device)
             x = x.to(device=device)
@@ -44,34 +60,32 @@ def train(model, train_loader, device,
     return model, training_stats
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # TODO: Get through CLI args
     epochs = 200
-    batch_size = 32
-    use_gpu = False
-    tile_size = (256, 256)
-
-    learning_rate = 0.01
-    weight_decay = 0.0005
+    batch_size = 8  # 8x8
+    # use_gpu = False
+    use_gpu = True
+    tile_size = (250, 250)
+    learning_rate = 1e-4
+    weight_decay = 0.001
     device = utils.device(use_gpu=use_gpu)
-
     model = FCNN()
-
-    train_loader = dataset.training_loader(batch_size=batch_size,
-                                           tile_size=tile_size)
-
-    model, stats = train(model=model,
-                         train_loader=train_loader,
-                         device=device,
-                         epochs=epochs,
-                         batch_size=batch_size,
-                         tile_size=tile_size,
-                         learning_rate = learning_rate,
-                         weight_decay = weight_decay)
+    train_loader = dataset.training_loader(batch_size=batch_size, tile_size=tile_size)
+    model, stats = train(
+        model=model,
+        train_loader=train_loader,
+        device=device,
+        epochs=epochs,
+        batch_size=batch_size,
+        tile_size=tile_size,
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+    )
 
     model_path = utils.save_weights_to_disk(model)
-    print('(i) Model saved at {}'.format(model_path))
-    loss_plot_path = './images/output/loss_plot_boxhill.png'
+    print("(i) Model saved at {}".format(model_path))
+    loss_plot_path = "./images/output/loss_plot_boxhill.png"
     stats.save_loss_plot(loss_plot_path)
-    print('(i) Loss plot saved at {}'.format(loss_plot_path))
+    print("(i) Loss plot saved at {}".format(loss_plot_path))
